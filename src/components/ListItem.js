88635392285c55273bf6edf76be1dda061cc1e8b
Scott DeVito <scottadevito@gmail.com>
2017-07-24 19:24:07 -0400

    Convert ListItem to functional component

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Image, Glyphicon } from 'react-bootstrap';
 
-class ListItem extends Component {
-  render() {
+const ListItem = ({ name}) => {
     return (
       <div className="List-Item">
         <Image src="https://pbs.twimg.com/profile_images/378800000508720698/c7723d7d8a07e4cf169e464716115abb.jpeg" className="AnimalImg" rounded />
-        {this.props.name}
+        {name}
         <Glyphicon glyph="remove" />
       </div>
     );
-  }
 }
 
 export default ListItem;
